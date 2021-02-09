2670b088c17d5e2899b86b0b928f4cfcc7bb7b8b
Jacky Huang <jacky_huang56@hotmail.com>
2021-02-09 14:33:55 -0700

    get semi working

--- httpclient.py
+++ httpclient.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# remove this later !/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
 # 
@@ -23,6 +23,7 @@ import socket
 import re
 # you may use urllib to encode data appropriately
 import urllib.parse
+from pprint import pprint
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -33,7 +34,12 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        host = urllib.parse.urlparse(url).hostname
+        port = urllib.parse.urlparse(url).port
+        if port == None:
+            port = 80
+        return host, port
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,13 +47,19 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        code = data.split("\r\n")[0]
+        code = code.split("HTTP/1.1 ")[1]
+        code = code.split(" ")[0]
+        return code
 
     def get_headers(self,data):
-        return None
+        headers = data.split("\r\n")[1]
+        headers = data.split("\r\n\r\n")[0]
+        return headers
 
     def get_body(self, data):
-        return None
+        body = data.split("\r\n\r\n")[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -69,12 +81,22 @@ class HTTPClient(object):
 
     def GET(self, url, args=None):
         code = 500
-        body = ""
+        host, port = self.get_host_port(url)
+        body = f'GET / HTTP/1.1\r\nHost: {host}\r\n\r\n'
+        self.connect(host, port)
+        self.sendall(body)
+        data = self.recvall(self.socket)
+
+        code = self.get_code(data)
+        headers = self.get_headers(data)
+        body = self.get_body(data)
+
         return HTTPResponse(code, body)
+
 
     def POST(self, url, args=None):
         code = 500
-        body = ""
+        body = f'POST /~hindle1/1.py HTTP/1.1\r\nHost: {url}\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 60\r\n\r\n'
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
@@ -90,6 +112,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        pprint(client.command( sys.argv[2], sys.argv[1] ))
     else:
-        print(client.command( sys.argv[1] ))
+        pprint(client.command( sys.argv[1] ))
